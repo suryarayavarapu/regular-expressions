87c2b6faf5e759f0ebe471356469066f1a982288
suryarayavarapu <rayavarapunagasuryakiran@gmail.com>
2026-02-18 21:17:43 +0530

    fgs

---
+++ jds.py
@@ -0,0 +1,11 @@
+s = "Geeks welcome to the Geek Kingdom!"
+
+if "Geek" in s:
+    print("Substring found!")
+else:
+    print("Substring not found!")
+
+if "For" in s:
+    print("Substring found!")
+else:
+    print("Substring not found!")
